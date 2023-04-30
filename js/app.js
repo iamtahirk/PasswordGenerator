d6e678a9156211e3442037c0874be751323bdcfc
Tahir <muhammadtahirk@gmail.com>
2023-04-30 23:29:09 +0500

    Password Generation Security
    Updated the passwords to use cryptographic library which is used to generate cryptographically secure random passwords.

--- js/app.js
+++ js/app.js
@@ -14,23 +14,19 @@ const numberCheck = document.getElementById('numbers');
 const symbolsCheck = document.getElementById('symbols');
 const generatePwdBtn = document.getElementById('pwdgeneratebtn');
 
-
 generatePwdBtn.addEventListener('click', (e)=> {
   e.preventDefault();
 
   if (lowerCaseCheck.checked || upperCaseCheck.checked || numberCheck.checked || symbolsCheck.checked) {
-    let stringPWD = [];
+    let passwords = [];
     for (let i = 0; i < 4; i++) {
-      stringPWD.push(generatePassword(parseInt(rangeSlider.value)))
+      passwords.push(generateRandomPassword(parseInt(rangeSlider.value)))
     }
 
-    // console.table(stringPWD, stringPWD[0].length, stringPWD[1].length, stringPWD[2].length, stringPWD[3].length);
-    // console.log(stringPWD[0].length, stringPWD[1].length, stringPWD[2].length, stringPWD[3].length);
-
-    passOneDisp.innerText = stringPWD[0];
-    passTwoDisp.innerText = stringPWD[1];
-    passThreeDisp.innerText = stringPWD[2];
-    passFourDisp.innerText = stringPWD[3];
+    passOneDisp.innerText = passwords[0];
+    passTwoDisp.innerText = passwords[1];
+    passThreeDisp.innerText = passwords[2];
+    passFourDisp.innerText = passwords[3];
   } else {
     alert ('Please check atleast one option to generate password.')
   }
@@ -40,19 +36,18 @@ rangeInputBox.addEventListener('change', () => {
   if (parseInt(rangeInputBox.value) > 25 || parseInt(rangeInputBox.value) < 1) {
     alert('Error: Value must be within 1-25.');
     setTimeout(() => {
-      rangeSlider.value = 8;
-      rangeInputBox.value = 8;
+      rangeSlider.value = 10;
+      rangeInputBox.value = 10;
     }, 300);
+    return;
   }
-  rangeSlider.value = rangeInputBox.value;
 });
 
 rangeSlider.addEventListener('input', () => {
   rangeInputBox.value = rangeSlider.value;
 });
 
-
-function generatePassword(length) {
+function generateRandomPassword(length) {
   let characterTypes = [];
   let password = "";
 
@@ -71,15 +66,20 @@ function generatePassword(length) {
 
   for (let i = 0; i < length; i++) {
     // choose a random character type from the characterTypes array
-    let randomCharType = characterTypes[Math.floor(Math.random() * characterTypes.length)];
+    // let randomCharType = characterTypes[Math.floor(Math.random() * characterTypes.length)];
+    // let randomCharType = characterTypes[Math.floor(crypto.randomInt(characterTypes.length))];
+    let randomCharType = characterTypes[Math.floor(window.crypto.getRandomValues(new Uint32Array(1))[0] / (0xffffffff + 1) * characterTypes.length)];
+
     // choose a random character from the chosen character type
-    let randomChar = randomCharType[Math.floor(Math.random() * randomCharType.length)];
+    // let randomChar = randomCharType[Math.floor(Math.random() * randomCharType.length)];
+    // let randomChar = randomCharType.charAt(crypto.randomInt(randomCharType.length));
+    let randomChar = randomCharType.charAt(Math.floor(window.crypto.getRandomValues(new Uint32Array(1))[0] / (0xffffffff + 1) * randomCharType.length));
+
     // add the random character to the password
     password += randomChar;
   }
   return password;
 }
-
 
 const copyClipBoard = (pass) => {
   const textarea = document.createElement('textarea');
@@ -95,37 +95,36 @@ const copyClipBoard = (pass) => {
 	textarea.select();
 	document.execCommand('copy');
 	textarea.remove();
-
-  // alert ('Password Copied.')
 }
-
 
 pwdDisplay.addEventListener('click', (e) => {
   e.preventDefault();
-  // console.log(e.target.getAttribute("class"));
 
-  /*
-  If condition is to avoid event delegation and not triggering the copied 
-  message's Background color rendering on whole layout if clicked other than pswd slots.
-  */
-  if (e.target.getAttribute("class") == 'pwd') {
-    if(!e.target.innerText || e.target.innerText == '•••') { 
-      // console.log('Error: Could not copy!!');
+  // Check if the clicked element is a password slot (with class 'pwd')
+  if (e.target.classList.contains("pwd")) {
+    // Get the password value from the clicked element
+    const password = e.target.innerText;
+
+    // If the password value is empty or already hidden, do nothing
+    if (password === '' || password === '•••') {
       return;
-    } else if (e.target && e.target.nodeName == "DIV") {
-      copyClipBoard(e.target.innerText);
-      
-      const div = e.target;
-      
-      const tooltip = document.createElement('div');
-        tooltip.classList.add('tooltip');
-        tooltip.innerHTML = 'Copied!';
-  
-        div.appendChild(tooltip);
-  
-        setTimeout(() => {
-          div.removeChild(tooltip);
-        }, 500);
     }
+
+    // Otherwise, copy the password to the clipboard
+    copyClipBoard(password);
+      
+    // Create a tooltip element to show 'Copied!' message
+    const tooltip = document.createElement('div');
+    tooltip.classList.add('tooltip');
+    tooltip.innerHTML = 'Copied!';
+    
+    // Append the tooltip element to the clicked password slot
+    const div = e.target;
+    div.appendChild(tooltip);
+
+    // Remove the tooltip after 0.5 seconds
+    setTimeout(() => {
+      div.removeChild(tooltip);
+    }, 500);
   }
 })
